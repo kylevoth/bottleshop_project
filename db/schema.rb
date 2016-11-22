76257b4aeb6bb52a4fe651ec1304fa71bfc25bbc
kylevoth <kyle_voth@hotmail.com>
2016-11-21 20:19:55 -0600

    Add and migrate new Type model and add to Active Admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121214008) do
+ActiveRecord::Schema.define(version: 20161122020415) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -61,6 +61,12 @@ ActiveRecord::Schema.define(version: 20161121214008) do
     t.decimal  "pst"
     t.decimal  "gst"
     t.decimal  "hst"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
